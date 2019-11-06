3fff6d65947e855ccacbfed7e07c9a3b09629713
Joanna May <joannamadisonmay@gmail.com>
2019-11-05 22:17:44 -0600

    Rename grammar statements

--- src/language/visitor.ts
+++ src/language/visitor.ts
@@ -1,9 +1,9 @@
 import { ProgramNode } from "../parser/nodes/program-node";
-import { VariableNode } from "../parser/nodes/variable-node";
+import { DeclarationNode } from "../parser/nodes/declaration-node";
 import { ValueNode } from "../parser/nodes/value-node";
 
 export abstract class Visitor {
 	public abstract visitProgramNode(node: ProgramNode): void;
-	public abstract visitVariableNode(node: VariableNode): void;
+	public abstract visitDeclarationNode(node: DeclarationNode): void;
 	public abstract visitValueNode(node: ValueNode): void;
 }
---
+++ src/parser/nodes/declaration-node.ts
@@ -0,0 +1,22 @@
+import { Visitor } from "../../language/visitor";
+import { Statement, Expression } from "../../language/node";
+
+export class DeclarationNode extends Statement {
+	/** Name of the variable */
+	public readonly name: string;
+	/** Type of the variable */
+	public readonly typeAnnotation: string[];
+	/** Value of the variable */
+	public readonly expr: Expression;
+
+	constructor(name: string, typeAnnotation: string[], expr: Expression) {
+		super();
+		this.name = name;
+		this.typeAnnotation = typeAnnotation;
+		this.expr = expr;
+	}
+
+	public accept(visitor: Visitor): void {
+		visitor.visitDeclarationNode(this);
+	}
+}
