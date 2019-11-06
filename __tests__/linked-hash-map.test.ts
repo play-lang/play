997f95929e2b14d5d5726de5ee3c24cba064f00a
Joseph Essin <josephessin@gmail.com>
2019-11-06 08:52:31 -0600

    Add map tests, fix broken file

--- __tests__/linked-hash-map.test.ts
+++ __tests__/linked-hash-map.test.ts
@@ -1,7 +1,34 @@
 import { LinkedHashMap } from "../src/common/linked-hash-map";
 
 describe("linked hash map", () => {
+	const map = new LinkedHashMap<string, number>();
+	it("should exist", () => {
+		expect(map).toBeInstanceOf(LinkedHashMap);
+	});
 	it("should initialize sensibly", () => {
-		const map = new LinkedHashMap<string, number>();
+		map.set("a", 1);
+		map.set("b", 2);
+		map.set("c", 3);
+	});
+	it("should be iterable", () => {
+		let i = 0;
+		const keys = ["a", "b", "c"];
+		const values = [1, 2, 3];
+		for (const [key, value] of map.entries()) {
+			expect(key).toEqual(keys[i]);
+			expect(value).toEqual(values[i]);
+			i++;
+		}
+		// See if the iterator resets itself
+		i = 0;
+		for (const [key, value] of map.entries()) {
+			expect(key).toEqual(keys[i]);
+			expect(value).toEqual(values[i]);
+			i++;
+		}
+	});
+	it("should have a description", () => {
+		expect(map.toString()).toEqual("[object Map]");
+		expect(map.description).toEqual("{a:1,b:2,c:3}");
 	});
 });
