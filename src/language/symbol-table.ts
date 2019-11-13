64de750f61038ad32ace699dde8e618b3099e07c
Joseph Essin <josephessin@gmail.com>
2019-11-12 22:43:39 -0600

    Update symbol table

--- src/language/symbol-table.ts
+++ src/language/symbol-table.ts
@@ -1,64 +1,57 @@
 import { Token, Describable } from "./token";
 import { LinkedHashMap } from "../common/linked-hash-map";
 
-export interface SymbolTableValue {
-	/** Position of the value within the owning scope */
-	ordinal: number;
-}
-
 /** Represents an entry in a symbol table */
-export class IdentifierSymbol implements SymbolTableValue {
+export class IdentifierSymbol {
 	/** Token for the identifier (to keep track of where it was first declared) */
 	public token: Token;
 	/** Type annotation of the identifier */
 	public typeAnnotation: string[];
-	public readonly ordinal: number;
-	constructor(token: Token, type: string[], ordinal: number) {
+	constructor(token: Token, typeAnnotation: string[]) {
 		this.token = token;
-		this.typeAnnotation = type;
-		this.ordinal = ordinal;
+		this.typeAnnotation = typeAnnotation;
 	}
 }
 
-export default class SymbolTable implements SymbolTableValue, Describable {
-	/** Number of child scopes */
-	public numScopes: number = 0;
-	/** Number of identifiers */
-	public numIdentifiers: number = 0;
+export default class SymbolTable implements Describable {
 	/** Parent scope, if any */
-	public readonly parentScope: SymbolTable | null;
-	/** Internal identifier for the scope name (for debugging the symbol table) */
-	public readonly scopeId: string;
-	public readonly ordinal: number;
+	public readonly enclosingScope: SymbolTable | undefined;
+
+	/** Child scopes */
+	public readonly scopes: SymbolTable[] = [];
+
+	/**
+	 * Number of entries "available" to be considered in-scope
+	 *
+	 * As the symbol table is visited, the available entries is incremented each
+	 * time an identifier is seen rather than re-registering or building a new
+	 * symbol table
+	 *
+	 * The number of available entries are used to determine if an identifier is
+	 * in scope in an enclosing scope as the enclosing scope could have declared
+	 * the identifier *after* the nested scope
+	 */
+	public available: number = 0;
 
 	/**
 	 * Maps identifier and scope id's to their respective IdentifierSymbols and
 	 * Scopes
 	 */
-	public entries: Map<string, SymbolTableValue> = new LinkedHashMap<
-		string,
-		SymbolTableValue
-	>();
+	public entries: LinkedHashMap<string, IdentifierSymbol> = new LinkedHashMap();
 
-	/** Current item in the scope tree */
-	protected currentKey: string = "";
-
-	constructor(
-		scopeId: string,
-		parentScope: SymbolTable | null = null,
-		ordinal: number = 0
-	) {
-		this.scopeId = scopeId;
-		this.parentScope = parentScope;
-		this.ordinal = ordinal;
+	constructor(enclosingScope?: SymbolTable) {
+		this.enclosingScope = enclosingScope;
 	}
 
 	/**
-	 * Total number of entries in the symbol table (including nested scopes and
-	 * symbols)
+	 * Reset the number of available entries for this scope and all of its
+	 * child scopes
 	 */
-	public get totalEntries(): number {
-		return this.numScopes + this.numIdentifiers;
+	public resetAvailable(): void {
+		this.available = -1;
+		for (const scope of this.scopes) {
+			scope.resetAvailable();
+		}
 	}
 
 	/**
@@ -67,14 +60,16 @@ export default class SymbolTable implements SymbolTableValue, Describable {
 	 *
 	 * @param id The identifier to check
 	 */
-	public identifierInScope(id: string): boolean {
-		if (this.entries.get(id) instanceof IdentifierSymbol) return true;
-		let parentScope = this.parentScope;
-		while (parentScope) {
-			if (parentScope.identifierInScope(id)) return true;
-			parentScope = parentScope.parentScope;
+	public idInScope(id: string): boolean {
+		if (
+			this.entries.get(id) instanceof IdentifierSymbol &&
+			this.entries.ordinal(id)! < this.available
+		) {
+			return true;
 		}
-		return false;
+		if (!this.enclosingScope) return false;
+		// Tail recursion lookup
+		return this.enclosingScope.idInScope(id);
 	}
 
 	/**
@@ -98,12 +93,8 @@ export default class SymbolTable implements SymbolTableValue, Describable {
 		// variable name as an outer scope
 		const key = token.lexeme;
 		if (this.entries.get(key)) return false;
-		this.entries.set(
-			key,
-			new IdentifierSymbol(token, typeAnnotation, this.totalEntries)
-		);
-		this.numIdentifiers++;
-		this.currentKey = key;
+		this.entries.set(key, new IdentifierSymbol(token, typeAnnotation));
+		this.available++;
 		return true;
 	}
 
@@ -118,17 +109,13 @@ export default class SymbolTable implements SymbolTableValue, Describable {
 	}
 
 	/**
-	 * Add a new scope at the end of the symbol table
+	 * Add a new child scope
 	 *
 	 * @returns The new scope
 	 */
 	public addScope(): SymbolTable {
-		// We can use an invalid identifier string for a scope key since
-		// identifiers can't contain operator symbols
-		this.currentKey = "SCOPE-" + String(this.numScopes);
-		const scope = new SymbolTable(this.currentKey, this, this.totalEntries);
-		this.entries.set(this.currentKey, scope);
-		this.numScopes++;
+		const scope = new SymbolTable(this);
+		this.scopes.push(scope);
 		return scope;
 	}
 
@@ -138,24 +125,29 @@ export default class SymbolTable implements SymbolTableValue, Describable {
 	}
 
 	protected _description(indent: number = 0): string {
-		const entries = Array.from(this.entries.values()).sort(
-			(a, b) => a.ordinal - b.ordinal
-		);
+		// Converts scope table to very concise json for testing
+		const entries = Array.from(this.entries.values());
 		let result = "";
-		entries.forEach(entry => {
-			result += "".padStart(indent * 2, " ");
-			if (entry instanceof SymbolTable) {
-				result += "Scope(" + entry.scopeId + ")\n";
-				result += entry._description(indent + 1);
-			} else if (entry instanceof IdentifierSymbol) {
-				result +=
-					"Id(`" +
-					entry.token.lexeme +
-					"`, `" +
-					entry.typeAnnotation.join(" ") +
-					"`)\n";
-			}
+		result += '{ "ids": [';
+		entries.forEach((entry, index) => {
+			const comma = index + 1 === entries.length ? "" : ", ";
+			result +=
+				'"Id(' +
+				String(index) +
+				", `" +
+				entry.token.lexeme +
+				"`, `" +
+				entry.typeAnnotation.join(" ") +
+				'`)"' +
+				comma;
 		});
-		return result;
+		result += "]" + (this.scopes.length > 0 ? ', "scopes": [' : "");
+		for (const [index, scope] of this.scopes.entries()) {
+			result += scope._description(indent + 1);
+			if (index + 1 !== this.scopes.length) result += ", ";
+		}
+		if (this.scopes.length > 0) result += "]";
+		result += "}";
+		return result.trim();
 	}
 }
