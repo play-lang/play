9819e5586cb1d3829462f2aab9cffc6dd06d9002
Joseph Essin <josephessin@gmail.com>
2019-11-17 23:28:06 -0600

    Fix tests

--- __tests__/unicode-escape.test.ts
+++ __tests__/unicode-escape.test.ts
@@ -2,16 +2,16 @@ import { escapeString, unescapeString } from "../src/common/unicode";
 
 describe("unicode escape", () => {
 	it("should escape strings correctly", () => {
-		expect(escapeString("abc🙄😘123\n\r")).toEqual(
-			"abc\\u{1f644}\\u{1f618}123\\n\\r"
+		expect(escapeString('abc🙄😘123\n\r\\"')).toEqual(
+			'abc\\u{1f644}\\u{1f618}123\\n\\r\\\\\\"'
 		);
 	});
 });
 
 describe("unicode unescape", () => {
 	it("should unescape strings correctly", () => {
-		expect(unescapeString("abc\\u{1f644}\\u{1f618}123\\n\\r")).toEqual(
-			"abc🙄😘123\n\r"
+		expect(unescapeString('abc\\u{1f644}\\u{1f618}123\\n\\r\\\\\\"')).toEqual(
+			'abc🙄😘123\n\r\\"'
 		);
 	});
 });
