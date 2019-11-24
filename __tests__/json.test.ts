168021fc75fe4db0cc8a67c7130cf96ce29ff20d
Joseph Essin <josephessin@gmail.com>
2019-11-24 12:58:20 -0600

    Fix error detection in parsing

--- __tests__/json.test.ts
+++ __tests__/json.test.ts
@@ -3,13 +3,9 @@ import { Play } from "../src/play";
 
 describe("json tree printer", () => {
 	it("should convert a simple AST to JSON", () => {
-		try {
-			const code = str`let myNum: num = 10 + 20 + 30`;
-			const json = Play.describeAstAsJSON(code);
-			console.log(JSON.stringify(json));
-		} catch (e) {
-			console.error(e);
-			throw e;
-		}
+		const code = str`let myNum: num = 10 + 20 + 30
+			myNum`;
+		const json = Play.describeAstAsJSON(code);
+		console.log(JSON.stringify(json));
 	});
 });
