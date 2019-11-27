609b5c7b2fb7463dc9679eb808196420fbb11496
Joanna May <joannamadisonmay@gmail.com>
2019-11-26 20:40:04 -0600

    Stub out preprocessor

--- src/play.ts
+++ src/play.ts
@@ -34,7 +34,7 @@ export class Play {
 	 * @returns The abstract syntax tree
 	 */
 	public static parse(code: string): AbstractSyntaxTree {
-		const parser = new Parser("code", code);
+		const parser = new Parser(code);
 		const ast = parser.parse();
 		if (parser.errors.length > 0) {
 			console.error(parser.errors);
