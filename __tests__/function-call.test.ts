df439fa9710bd200a9b0c54720a4644bfc459478
Joanna May <joannamadisonmay@gmail.com>
2019-12-02 11:54:44 -0600

    Update some tests before refactoring for call frames

--- __tests__/function-call.test.ts
+++ __tests__/function-call.test.ts
@@ -1,66 +1,67 @@
 import { str } from "../shared/test-utils";
 import { Play } from "../src/play";
 describe("simple function call", () => {
-	// it("should parse an action", () => {
-	// 	const code = str`
-	// 		action myAction(): str {
-	// 			return
-	// 			return 3 + 4
-	// 		}
-	// 	`;
-	// 	const result = Play.describeAstAsJSON(code);
-	// 	expect(result).toEqual({
-	// 		type: "program",
-	// 		start: 0,
-	// 		end: 44,
-	// 		statements: [
-	// 			{
-	// 				type: "action-decl",
-	// 				start: 0,
-	// 				end: 44,
-	// 				typeAnnotation: ["str"],
-	// 				parameters: [],
-	// 				block: {
-	// 					type: "block",
-	// 					start: 23,
-	// 					end: 44,
-	// 					isActionBlock: true,
-	// 					statements: [
-	// 						{
-	// 							type: "return",
-	// 							start: 25,
-	// 							end: 31,
-	// 						},
-	// 						{
-	// 							type: "return-value",
-	// 							start: 43,
-	// 							end: 44,
-	// 							value: {
-	// 								type: "binary-expr",
-	// 								start: 39,
-	// 								end: 44,
-	// 								lhs: {
-	// 									type: "literal",
-	// 									start: 39,
-	// 									end: 40,
-	// 									literalType: "Number",
-	// 									literalValue: "3",
-	// 								},
-	// 								rhs: {
-	// 									type: "literal",
-	// 									start: 43,
-	// 									end: 44,
-	// 									literalType: "Number",
-	// 									literalValue: "4",
-	// 								},
-	// 							},
-	// 						},
-	// 					],
-	// 				},
-	// 			},
-	// 		],
-	// 	});
-	// });
+	it("should parse an action", () => {
+		const code = str`
+			action myAction(): str {
+				return
+				return 3 + 4
+			}
+		`;
+		const result = Play.describeAstAsJSON(code);
+		expect(result).toEqual({
+			type: "program",
+			start: 0,
+			end: 44,
+			statements: [
+				{
+					type: "action-decl",
+					start: 0,
+					end: 44,
+					typeAnnotation: ["str"],
+					parameters: [],
+					parameterTypes: [],
+					block: {
+						type: "block",
+						start: 23,
+						end: 44,
+						isActionBlock: true,
+						statements: [
+							{
+								type: "return",
+								start: 25,
+								end: 31,
+							},
+							{
+								type: "return-value",
+								start: 43,
+								end: 44,
+								value: {
+									type: "binary-expr",
+									start: 39,
+									end: 44,
+									lhs: {
+										type: "literal",
+										start: 39,
+										end: 40,
+										literalType: "Number",
+										literalValue: "3",
+									},
+									rhs: {
+										type: "literal",
+										start: 43,
+										end: 44,
+										literalType: "Number",
+										literalValue: "4",
+									},
+								},
+							},
+						],
+					},
+				},
+			],
+		});
+	});
 
 	// it("should generate the right bytecode", () => {
 	// 	const code = str`
@@ -105,19 +106,19 @@ describe("simple function call", () => {
 	// 	console.log(result);
 	// });
 
-	it("should handle local variables with recursion", () => {
-		const code = str`
-			first(0)
-			action first(a: num): num {
-				let b: num = 10
-				let c: num = a > 0 ? a : first(a + b)
-				return c
-			}
-		`;
-		const desc = Play.disassemble(code);
-		console.log(desc);
-		const result = Play.run(code);
-		expect(result.value.value).toBe(10);
-		console.log(result);
-	});
+	// it("should handle local variables with recursion", () => {
+	// 	const code = str`
+	// 		first(0)
+	// 		action first(a: num): num {
+	// 			let b: num = 10
+	// 			let c: num = a > 0 ? a : first(a + b)
+	// 			return c
+	// 		}
+	// 	`;
+	// 	const desc = Play.disassemble(code);
+	// 	console.log(desc);
+	// 	const result = Play.run(code);
+	// 	expect(result.value.value).toBe(10);
+	// 	console.log(result);
+	// });
 });
--- __tests__/parser.test.ts
+++ __tests__/parser.test.ts
@@ -3,6 +3,88 @@ import { Play } from "../src/play";
 describe("parser", () => {
 	it("should parse", () => {
 		const tree = Play.describeAstAsJSON("a + ((b - c) * 2)^-2 / 3 % 4");
-		expect(tree).toEqual({});
+		expect(tree).toEqual({
+			type: "program",
+			start: 0,
+			end: 28,
+			statements: [
+				{
+					type: "binary-expr",
+					start: 0,
+					end: 28,
+					lhs: { type: "action-ref", start: 0, end: 1, actionName: "a" },
+					rhs: {
+						type: "binary-expr",
+						start: 6,
+						end: 28,
+						lhs: {
+							type: "binary-expr",
+							start: 6,
+							end: 24,
+							lhs: {
+								type: "binary-expr",
+								start: 6,
+								end: 20,
+								lhs: {
+									type: "binary-expr",
+									start: 6,
+									end: 16,
+									lhs: {
+										type: "binary-expr",
+										start: 6,
+										end: 11,
+										lhs: {
+											type: "action-ref",
+											start: 6,
+											end: 7,
+											actionName: "b",
+										},
+										rhs: {
+											type: "action-ref",
+											start: 10,
+											end: 11,
+											actionName: "c",
+										},
+									},
+									rhs: {
+										type: "literal",
+										start: 15,
+										end: 16,
+										literalType: "Number",
+										literalValue: "2",
+									},
+								},
+								rhs: {
+									type: "prefix-expr",
+									start: 18,
+									end: 20,
+									rhs: {
+										type: "literal",
+										start: 19,
+										end: 20,
+										literalType: "Number",
+										literalValue: "2",
+									},
+								},
+							},
+							rhs: {
+								type: "literal",
+								start: 23,
+								end: 24,
+								literalType: "Number",
+								literalValue: "3",
+							},
+						},
+						rhs: {
+							type: "literal",
+							start: 27,
+							end: 28,
+							literalType: "Number",
+							literalValue: "4",
+						},
+					},
+				},
+			],
+		});
 	});
 });
