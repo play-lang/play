66a2d7b17e98095187cfe4121a82ad8df8ec49b4
Joseph Essin <josephessin@gmail.com>
2019-12-11 11:03:23 -0600

    Rename type system file, remove junk file

--- src/language/types/primitives.ts
+++
@@ -1,5 +0,0 @@
-import { Primitive, PrimitiveType } from "./type-system-2";
-
-export const Str = new PrimitiveType(Primitive.Str);
-export const Num = new PrimitiveType(Primitive.Num);
-export const Bool = new PrimitiveType(Primitive.Bool);
