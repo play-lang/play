5b72daaabd28b19681ffeb823607aa5948796058
Joseph Essin <josephessin@gmail.com>
2020-01-08 11:27:57 -0600

    Add context label printing to disassembler

--- __tests__/disassembler.test.ts
+++ __tests__/disassembler.test.ts
@@ -13,24 +13,25 @@ describe("disassembler", () => {
 	0005	Number	6
 
 .CODE
+LABEL_0000: ; (CONTEXT)
 	0000	               FALSE
-	0001	        JUMPFALSEPOP	LABEL_0000
+	0001	        JUMPFALSEPOP	LABEL_0001
 	0003	            CONSTANT	(0)	= 1
 	0005	            CONSTANT	(1)	= 2
 	0007	                 ADD
-	0008	                JUMP	LABEL_0002
-LABEL_0000:
+	0008	                JUMP	LABEL_0003
+LABEL_0001:
 	0010	                TRUE
-	0011	        JUMPFALSEPOP	LABEL_0001
+	0011	        JUMPFALSEPOP	LABEL_0002
 	0013	            CONSTANT	(2)	= 3
 	0015	            CONSTANT	(3)	= 4
 	0017	                 ADD
-	0018	                JUMP	LABEL_0002
-LABEL_0001:
+	0018	                JUMP	LABEL_0003
+LABEL_0002:
 	0020	            CONSTANT	(4)	= 5
 	0022	            CONSTANT	(5)	= 6
 	0024	                 ADD
-LABEL_0002:
+LABEL_0003:
 	0025	              RETURN
 `;
 		expect(dis).toBe(expected);
