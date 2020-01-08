4debd960c32500d1b0bd71c2f718df9964ab2051
Joanna May <joannamadisonmay@gmail.com>
2020-01-08 08:48:37 -0600

    Fix label generation

--- __tests__/disassembler.test.ts
+++ __tests__/disassembler.test.ts
@@ -2,7 +2,7 @@ import { Play } from "../src/play";
 
 describe("disassembler", () => {
 	test("should disassemble labels", () => {
-		const code = "return false ? 1+2 : (3+4 ? 5+6 : 7+8)";
+		const code = "return false ? 1+2 : (true ? 3+4 : 5+6)";
 		const dis = Play.disassemble(code);
 		console.log(dis);
 		const dis2 = Play.disassemble2(code);
