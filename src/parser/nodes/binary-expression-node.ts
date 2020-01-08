74d2523e64bc788c0e5808d174d292fab5601cd3
Joanna May <joannamadisonmay@gmail.com>
2020-01-08 16:34:30 -0600

    Update type checker to check binary expressions

--- src/parser/nodes/binary-expression-node.ts
+++ src/parser/nodes/binary-expression-node.ts
@@ -2,7 +2,13 @@ import { Expression } from "src/language/node";
 import { TokenLike } from "src/language/token";
 import { TokenType } from "src/language/token-type";
 import { Environment } from "src/language/types/environment";
-import { constructType, ErrorType, Type } from "src/language/types/type-system";
+import {
+	Bool,
+	ErrorType,
+	Num,
+	Str,
+	Type,
+} from "src/language/types/type-system";
 import { Visitor } from "src/language/visitor";
 
 export class BinaryExpressionNode extends Expression {
@@ -16,9 +22,6 @@ export class BinaryExpressionNode extends Expression {
 	}
 
 	public type(env: Environment): Type {
-		const bool = constructType(["bool"]);
-		const num = constructType(["num"]);
-		const str = constructType(["str"]);
 		switch (this.operatorType) {
 			case TokenType.EqualEqual:
 			case TokenType.BangEqual:
@@ -26,7 +29,7 @@ export class BinaryExpressionNode extends Expression {
 			case TokenType.LessThanEqual:
 			case TokenType.GreaterThan:
 			case TokenType.GreaterThanEqual:
-				return bool;
+				return Bool;
 			case TokenType.Plus:
 				// Plus is overloaded to support string concatenation
 				// and numeric addition
@@ -34,24 +37,70 @@ export class BinaryExpressionNode extends Expression {
 				// We check for string concatenation by checking if the lhs and rhs
 				// are string types
 				if (
-					this.lhs.type(env).equivalent(str) &&
-					this.rhs.type(env).equivalent(str)
+					this.lhs.type(env).equivalent(Str) &&
+					this.rhs.type(env).equivalent(Str)
 				) {
-					return str;
+					return Str;
+				} else if (
+					this.lhs.type(env).equivalent(Num) &&
+					this.rhs.type(env).equivalent(Num)
+				) {
+					return Num;
 				}
-				return num;
+				return new ErrorType(false);
 			case TokenType.Minus:
 			case TokenType.Asterisk:
 			case TokenType.Slash:
 			case TokenType.Percent:
 			case TokenType.Caret:
-				return num;
+				return Num;
 		}
 		return new ErrorType(false);
 	}
 
+	/**
+	 * Provides a string description of what the binary operator does for
+	 * type checking error reporting
+	 * @param env Type checking environment
+	 */
+	public action(env: Environment): string {
+		switch (this.operatorType) {
+			case TokenType.EqualEqual:
+			case TokenType.BangEqual:
+			case TokenType.LessThan:
+			case TokenType.LessThanEqual:
+			case TokenType.GreaterThan:
+			case TokenType.GreaterThanEqual:
+				return "compare";
+			case TokenType.Plus:
+				// Plus is overloaded to support string concatenation
+				// and numeric addition
+				//
+				// We check for string concatenation by checking if the lhs and rhs
+				// are string types
+				if (
+					this.lhs.type(env).equivalent(Str) &&
+					this.rhs.type(env).equivalent(Str)
+				) {
+					return "concatenate";
+				}
+				return "add";
+			case TokenType.Minus:
+				return "subtract";
+			case TokenType.Asterisk:
+				return "multiply";
+			case TokenType.Slash:
+				return "divide";
+			case TokenType.Percent:
+				return "take the remainder";
+			case TokenType.Caret:
+				return "raise to the power";
+		}
+		return "(invalid operation)";
+	}
+
 	// MARK: Visitor
 	public accept(visitor: Visitor): void {
-		visitor.visitBinaryExpressionNode?.(this);
+		visitor.visitBinaryExpressionNode(this);
 	}
 }
