4df034b9f0e46fa1ed5c3b2dd969ab46e2c24bd2
Joseph Essin <josephessin@gmail.com>
2020-01-22 16:32:32 -0600

    Add symbol table stub

---
+++ src/language/symbol-table.ts
@@ -0,0 +1,36 @@
+import { Scope } from "./scope";
+
+export class SymbolTable {
+	private _scope: Scope;
+
+	/** Index of the next child scope to visit for each scope level */
+	private childScopeIndices: number[] = [0];
+	/** Number of scopes deep we are--used as an index to childScopeIndices */
+	private scopeDepth: number = 0;
+
+	/** Current scope */
+	public get scope(): Scope {
+		return this._scope;
+	}
+
+	constructor(
+		/** Global scope */
+		public readonly globalScope: Scope
+	) {
+		this._scope = globalScope;
+	}
+
+	/** Enter the next child scope */
+	public enterScope(): void {
+		const childScopeIndex = this.childScopeIndices[this.scopeDepth++]++;
+		this.childScopeIndices.push(0);
+		this._scope = this.scope.scopes[childScopeIndex];
+	}
+
+	/** Go back to the parent scope */
+	public exitScope(): void {
+		this.scopeDepth--;
+		this.childScopeIndices.pop();
+		this._scope = this.scope.enclosingScope || this.globalScope;
+	}
+}
