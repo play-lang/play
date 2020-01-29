ada075d0dfcc67e8fdb9361252a4bc64ff706192
Joanna May <joannamadisonmay@gmail.com>
2020-01-28 22:21:35 -0600

    Clean up tests (again)

--- __tests__/disassembler.test.ts
+++ __tests__/disassembler.test.ts
@@ -1,47 +1,14 @@
 import { disassembleFile, disassembleFinalFile } from "../shared/test-utils";
-// import { Play } from "../src/play";
 
 describe("disassembler", () => {
-	// 	test("should disassemble labels", () => {
-	// 		const code = "return false ? 1+2 : (true ? 3+4 : 5+6)";
-	// 		const dis = Play.disassemble(code);
-	// 		const expected = `.CONSTANTS
-	// 	0000	Number	1
-	// 	0001	Number	2
-	// 	0002	Number	3
-	// 	0003	Number	4
-	// 	0004	Number	5
-	// 	0005	Number	6
-
-	// .CODE
-	// LABEL_0000: ; (CONTEXT) 0 LOCAL
-	// 	0000	               FALSE
-	// 	0001	        JUMPFALSEPOP	LABEL_0001
-	// 	0003	            CONSTANT	(0)	= 1
-	// 	0005	            CONSTANT	(1)	= 2
-	// 	0007	                 ADD
-	// 	0008	                JUMP	LABEL_0003
-	// LABEL_0001:
-	// 	0010	                TRUE
-	// 	0011	        JUMPFALSEPOP	LABEL_0002
-	// 	0013	            CONSTANT	(2)	= 3
-	// 	0015	            CONSTANT	(3)	= 4
-	// 	0017	                 ADD
-	// 	0018	                JUMP	LABEL_0003
-	// LABEL_0002:
-	// 	0020	            CONSTANT	(4)	= 5
-	// 	0022	            CONSTANT	(5)	= 6
-	// 	0024	                 ADD
-	// LABEL_0003:
-	// 	0025	              RETURN
-	// `;
-	// 		expect(dis).toBe(expected);
-	// 	});
-	// test("should disassemble multiple contexts", () => {});
 	test("should disassemble multiple linked contexts", async () => {
 		const dis = await disassembleFile("fib12.play");
-		console.log(dis);
+		expect(dis).toBe(
+			".CONSTANTS\n\t0000\tnumber\t1\n\t0001\tnumber\t2\n\t0002\tnumber\t12\n\n.CODE\n; Context (main)\n; 0 locals\nlabel_0000:\n\t0000\t               const\t 0002\t; value 12\n\t0002\t                load\t-1\t; context fib\n\t0004\t                call\t 0001\n\t0006\t              return\n; Context fib\n; 1 local\nlabel_0001:\n\t0000\t                 get\t 0000\n\t0002\t               const\t 0000\t; value 1\n\t0004\t           lessequal\n\t0005\t         jmpfalsepop\t 0004\t; label_0002 (instr  0011) \n\t0007\t                 get\t 0000\n\t0009\t                 jmp\t 0019\t; label_0003 (instr  0030) \nlabel_0002:\n\t0011\t                 get\t 0000\n\t0013\t               const\t 0000\t; value 1\n\t0015\t                 sub\n\t0016\t                load\t-1\t; context fib\n\t0018\t                call\t 0001\n\t0020\t                 get\t 0000\n\t0022\t               const\t 0001\t; value 2\n\t0024\t                 sub\n\t0025\t                load\t-1\t; context fib\n\t0027\t                call\t 0001\n\t0029\t                 add\nlabel_0003:\n\t0030\t              return\n"
+		);
 		const disFinal = await disassembleFinalFile("fib12.play");
-		console.log(disFinal);
+		expect(disFinal).toBe(
+			".CONSTANTS\n\t0000\tnumber\t1\n\t0001\tnumber\t2\n\t0002\tnumber\t12\n\n.CODE\n; Context (main)\n; 0 locals\nlabel_0000:\n\t0000\t               const\t 0002\t; value 12\n\t0002\t                load\t 0007\t; label_0001 context fib\n\t0004\t                call\t 0001\n\t0006\t              return\n; Context fib\n; 1 local\nlabel_0001:\n\t0007\t                 get\t 0000\n\t0009\t               const\t 0000\t; value 1\n\t0011\t           lessequal\n\t0012\t         jmpfalsepop\t 0004\t; label_0002 (instr  0018) \n\t0014\t                 get\t 0000\n\t0016\t                 jmp\t 0019\t; label_0003 (instr  0037) \nlabel_0002:\n\t0018\t                 get\t 0000\n\t0020\t               const\t 0000\t; value 1\n\t0022\t                 sub\n\t0023\t                load\t 0007\t; label_0001 context fib\n\t0025\t                call\t 0001\n\t0027\t                 get\t 0000\n\t0029\t               const\t 0001\t; value 2\n\t0031\t                 sub\n\t0032\t                load\t 0007\t; label_0001 context fib\n\t0034\t                call\t 0001\n\t0036\t                 add\nlabel_0003:\n\t0037\t              return\n"
+		);
 	});
 });
