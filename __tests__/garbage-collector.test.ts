c6db810194bd75a45c53ce95108e167863138841
Joseph Essin <josephessin@gmail.com>
2020-02-08 12:52:48 -0600

    Add some test utilities to gc

--- __tests__/garbage-collector.test.ts
+++ __tests__/garbage-collector.test.ts
@@ -9,12 +9,26 @@ describe("garbage collector", () => {
 	});
 	test("allocate", () => {
 		const gc = new GarbageCollector();
-		const a1 = gc.alloc([new RuntimeValue(RuntimeType.Number, 1)], []);
-		const a2 = gc.alloc(
-			[new RuntimeValue(RuntimeType.Number, 1)],
-			[new RuntimeValue(RuntimeType.Pointer, a1)]
-		);
+		const v1 = new RuntimeValue(RuntimeType.Number, 1);
+		const a1 = gc.alloc([v1], []);
+		const roots = [new RuntimeValue(RuntimeType.Pointer, a1)];
+		const v2 = new RuntimeValue(RuntimeType.Number, 2);
+		const a2 = gc.alloc([v2], roots);
+		// Check addresses, they should be consecutive since consecutive allocations
+		// without a collection
 		expect(a1).toBe(0);
 		expect(a2).toBe(1);
+		// Force collection
+		gc.collectAll(roots);
+		// There should only be one surviving thing on the heap
+		expect(gc.heap).toHaveLength(1);
+		// The surviving value should be equivalent to the value we originally
+		// gave it
+		expect(veq(gc.heap[0].values[0], v1)).toBe(true);
 	});
 });
+
+/** Check RuntimeValue equality */
+function veq(lhs: RuntimeValue, rhs: RuntimeValue): boolean {
+	return lhs.type === rhs.type && lhs.value === rhs.value;
+}
