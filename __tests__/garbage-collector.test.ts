42822e3af07736134b8ad59b6696c57e754c90f2
Joseph Essin <josephessin@gmail.com>
2020-02-12 20:32:39 -0600

    Fix test

--- __tests__/garbage-collector.test.ts
+++ __tests__/garbage-collector.test.ts
@@ -1,7 +1,9 @@
 import { GarbageCollector } from "../src/vm/garbage-collector";
-import { RuntimeType } from "../src/vm/runtime-type";
-import { RuntimeValue } from "../src/vm/runtime-value";
+// import { RuntimeType } from "../src/vm/runtime-type";
+// import { RuntimeValue } from "../src/vm/runtime-value";
 
 describe("garbage collector", () => {
-	test("initialize", () => {});
+	test("initialize", () => {
+		expect(new GarbageCollector()).toBeInstanceOf(GarbageCollector);
+	});
 });
