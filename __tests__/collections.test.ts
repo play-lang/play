4efb460d055b33a330a95ede15a81f22e0e83454
Joseph Essin <josephessin@gmail.com>
2020-02-19 10:11:32 -0600

    Test type-checker for variable assignment

--- __tests__/collections.test.ts
+++ __tests__/collections.test.ts
@@ -4,18 +4,20 @@ describe("collections", () => {
 	describe("lists", () => {
 		test("index into list literal", () => {
 			const code = "return [1, 2, 3][2] // x = 3";
-			console.log(Play.disassemble(code));
 			expect(Play.run(code).value.value).toBe(3);
 		});
 		test("assign into list pointer", () => {
 			const code = `
 var x = [1, 2, 3]
+x = [4, 5, 6]
 x[0] = -100
 return x[0]
 `;
-			console.log(Play.disassemble(code));
 			expect(Play.run(code).value.value).toBe(-100);
 		});
-		test("should fail to assign to constant list?", () => {});
+		test("should fail to assign to constant list?", () => {
+			const code = `let x = [1, 2, 3]\nx = [4, 5, 6]`;
+			expect(Play.check(code)).toHaveLength(1);
+		});
 	});
 });
