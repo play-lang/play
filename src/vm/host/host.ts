58bbad2d6babb2e1cb5a3c1906c628efe5ad899c
Joanna May <joannamadisonmay@gmail.com>
2020-02-20 22:47:16 -0600

    Add beginning of extension host

---
+++ src/vm/host/host.ts
@@ -0,0 +1,31 @@
+import { nativeFunctions, NativeFunction } from "src/vm/host/native-functions";
+
+/**
+ * Represents a virtual machine host which contains information about
+ * native and extension functions
+ *
+ * Native functions are built-in by the virtual machine and are a part of the
+ * Play programming language
+ *
+ * Extension functions are provided by the host environment utilizing the
+ * Play virtual machine with the extensions api's
+ */
+export class VirtualMachineHost {
+	/** Functions that can be executed by the virtual machine */
+	public readonly functions: NativeFunction[];
+	/**
+	 * The number of native functions provided (functions added after this in the
+	 * function list are extension functions)
+	 */
+	public readonly numNativeFunctions: number;
+
+	/**
+	 *
+	 * @param extendedFunctions Extension functions available to the virtual
+	 * machine
+	 */
+	constructor(extendedFunctions: NativeFunction[]) {
+		this.numNativeFunctions = nativeFunctions.length;
+		this.functions = [...nativeFunctions, ...extendedFunctions];
+	}
+}
