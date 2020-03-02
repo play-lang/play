0ce056478ccea473c1639d1c91d24d2ab052eaf5
Joseph Essin <josephessin@gmail.com>
2020-03-02 17:20:08 -0600

    Fix to avoid confusing model parameters and properties

--- src/parser/nodes/model-node.ts
+++ src/parser/nodes/model-node.ts
@@ -4,40 +4,43 @@ import { TokenLike } from "src/language/token";
 import { Environment } from "src/language/types/environment";
 import { ModelType, Type } from "src/language/types/type-system";
 import { Visitor } from "src/language/visitor";
+import { VariableDeclarationNode } from "src/parser/nodes/variable-declaration-node";
 
 export class ModelNode extends Expression {
 	constructor(
 		token: TokenLike,
 		public readonly name: string,
 
-		/** Instance properties for this model */
-		public readonly properties: PropertyInfo[]
+		/** Initialization parameters (similar to Kotlin class) */
+		public readonly initParams: PropertyInfo[],
+		/** Child property declaration nodes */
+		public readonly propertyDeclarations: VariableDeclarationNode[]
 	) {
 		super(token, token.pos, token.end);
 	}
 
 	public setState(state: NodeState): void {
 		this.state = state;
-		this.properties.forEach(property => {
-			property.defaultValue?.setState({
+		this.initParams.forEach(param => {
+			param.defaultValue?.setState({
 				...state,
 				parent: this,
 				isDead: false,
-				isLast: property === this.properties[this.properties.length - 1],
+				isLast: param === this.initParams[this.initParams.length - 1],
 			});
 		});
 	}
 
 	public type(env: Environment): Type {
-		// Construct a type for each property
-		const propertyTypeMap: Map<string, Type> = new Map();
-		for (const property of this.properties) {
-			propertyTypeMap.set(
-				property.name,
-				Type.construct(property.typeAnnotation, !property.isImmutable)
-			);
+		// Examine the child variable declaration nodes to determine our
+		// property types
+		const propertyTypes: Map<string, Type> = new Map();
+		for (const decl of this.propertyDeclarations) {
+			const propertyType = decl.type(env);
+			const propertyName = decl.variableName;
+			propertyTypes.set(propertyName, propertyType);
 		}
-		return new ModelType(this.name, propertyTypeMap, [], [], []);
+		return new ModelType(this.name, propertyTypes, [], [], []);
 	}
 
 	public accept(visitor: Visitor): void {
