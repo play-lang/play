fc727991598062e688fa6cc5841c096f4f40b9b4
Joanna May <joannamadisonmay@gmail.com>
2020-04-07 12:18:00 -0500

    Reorganize imports a little bit

--- __tests__/path-utility.test.ts
+++ __tests__/path-utility.test.ts
@@ -1,20 +1,22 @@
-import { directory, filename } from "../src/common/path-utility";
+import { PathUtil } from "../src/common/path-util";
 describe("path utility", () => {
 	test("should extract filenames", () => {
-		expect(filename("")).toBe("");
-		expect(filename("file.txt")).toBe("file.txt");
-		expect(filename("some/folder/here with spaces/file.txt")).toBe("file.txt");
-		expect(filename("some/folder/here with spaces/file-no ext")).toBe(
+		expect(PathUtil.filename("")).toBe("");
+		expect(PathUtil.filename("file.txt")).toBe("file.txt");
+		expect(PathUtil.filename("some/folder/here with spaces/file.txt")).toBe(
+			"file.txt"
+		);
+		expect(PathUtil.filename("some/folder/here with spaces/file-no ext")).toBe(
 			"file-no ext"
 		);
 	});
 	test("should extract folder names", () => {
-		expect(directory("")).toBe("/");
-		expect(directory("file")).toBe("/");
-		expect(directory("a/very/long/folder/parent folder/file")).toBe(
+		expect(PathUtil.directory("")).toBe("/");
+		expect(PathUtil.directory("file")).toBe("/");
+		expect(PathUtil.directory("a/very/long/folder/parent folder/file")).toBe(
 			"a/very/long/folder/parent folder/"
 		);
-		expect(directory("//some/folder/with/extra/slashes//")).toBe(
+		expect(PathUtil.directory("//some/folder/with/extra/slashes//")).toBe(
 			"some/folder/with/extra/slashes/"
 		);
 	});
